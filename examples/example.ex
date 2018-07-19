b0b631b7c06e1d4dd345e99b4c54b6962570f295
sophyc <sophy.cao@derivco.se>
2018-07-19 19:19:23 +0200

    wip: add example and continue parsing ast

---
+++ examples/example.ex
@@ -0,0 +1,8 @@
+defmodule Example do
+  defmodule Example.Foo do
+    def func(a, b), do: a + b
+  end
+end
+defmodule ExampleB do
+  def func(t), do: :ok
+end
--- lib/spec_generator/parser.ex
+++ lib/spec_generator/parser.ex
@@ -3,10 +3,27 @@ defmodule SpecGenerator.Parser do
     {:ok, _} = GenServer.start_link(SpecGenerator.Generator, [], name: :generator)
     code_str = File.read!(file_path)
     abstract = Code.string_to_quoted!(code_str)
-    parse({code_str, abstract}, "", &continue_parse/2)
+    code_str_list = code_str |> String.split("\n")
+    parsed_abstract = parse_abstract(abstract, [])
+ #   generated = parse({code_str_list, abstract}, "")
   end
 
-  defp parse({code_str, abstract}, generated, function) do
-    #TBC
+  defp parse_abstract([], parsed), do: parsed
+  defp parse_abstract([h|t], parsed), do: parse_abstract(t, parse_abstract(h, parsed))
+  defp parse_abstract({:defmodule, _meta, cont}, parsed), do: parse_abstract(cont, parsed)
+  defp parse_abstract({:def, meta, cont}, parsed), do: [parse_fucntion({meta, cont})|parsed]
+  defp parse_abstract({:defp, _meta, _cont}, parsed), do: parsed
+
+  defp parse_fucntion({meta, cont}) do
+    line_n = Keyword.get(meta, :line)
+    {func_name, args} = parse_function(cont)
+    {line_n, {func_name, args}}
   end
+
+  defp parse_function([{func_name, _func_meta, args}]), do: {func_name, parse_args(args)}
+
+  defp parse_args([]), do: []
+  defp parse_args([h|t]), do: [parse_args(h)|parse_args(t)]
+  defp parse_args({arg_name, _meta, nil}), do: {arg_name, nil}
+  defp parse_args({arg_name, _meta, value}), do: {arg_name, value}
 end
